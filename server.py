dd9dca8c4d1229a44b79973955b9702ba9dd5631
Trausti Sæmundsson <trausti@greenqloud.com>
2011-06-27 10:49:49 +0000

    tths

--- server.py
+++ server.py
@@ -6,7 +6,10 @@ import logging
 import json
 import uuid
 import yaml
+import copy
+import time
 
+from tornado.web import authenticated
 from tornado.ioloop import IOLoop, PeriodicCallback
 from tornado.web import RequestHandler, Application
 from tornado.websocket import WebSocketHandler
@@ -63,8 +66,10 @@ class Host(object):
                 'key': self.id})
             self.numcpus += o.numcpus
             self.mem += o.mem
+            return False
         else:
             self.vms.append(item)
+            return True
 
 
 def send_message(data):
@@ -96,31 +101,84 @@ def create_host():
     status = functools.partial(status_message, _key)
     PeriodicCallback(status, 3 * 1000, ioloop).start()
 
+def delete_host(_key):
+    logging.info('deleting host')
+    HOSTS.pop(_key)
 
 def create_vm(_key, so):
-    logging.info('added vm')
     vm = {'so': so, 'key': str(uuid.uuid4())}
-    HOSTS.get(_key).append(vm)
-    send_message({'key': _key,
-        'vm': vm, 'action': 'vm'})
+    if HOSTS[_key].append(vm):
+        logging.info('added vm')
+        send_message({'key': _key,
+            'vm': vm, 'action': 'vm'})
+    else:
+        logging.info('insufficient capacity on host '+_key+" for offering "+so)
 
+def delete_vm(_key,_vm_key):
+    if HOSTS.get(_key):
+        for virtual_machine in HOSTS[_key]:
+            if virtual_machine['key']==_vm_key:
+                so=virtual_machine['so']
+                o = offerings_lookup[so]
+                HOSTS[_key].vms.remove(virtual_machine)
+                HOSTS[_key].numcpus+=o.numcpus
+                HOSTS[_key].mem+=o.mem
+                logging.info('deleted vm')
+                break
 
-class MainHandler(RequestHandler):
+class BaseHandler(RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+class MainHandler(BaseHandler):
     def get(self):
+        if not self.current_user=="godzilla":
+            self.redirect("/login")
+            return
         self.render("index.html",
             title="Host simulator",
             path='localhost:8888',
             offerings=offerings)
-
     def post(self):
+        if not self.current_user=="gogo":
+            self.redirect("/login")
+            return
         action = self.request.arguments['action'][0]
 
         if action == 'host':
             create_host()
+        if action == 'logout':
+            self.clear_all_cookies()
+            self.redirect("/login")
+            return
+        elif action == 'delete_host':
+            try:
+                delete_host(self.request.arguments['host'][0])
+            except:
+                pass
         elif action == 'vm':
             host = self.request.arguments['host'][0]
             so = self.request.arguments['so'][0]
             create_vm(host, so)
+        elif action == 'delete_vm':
+            host = self.request.arguments['host'][0]
+            vmid = self.request.arguments['vmid'][0]
+            try:
+                delete_vm(host, vmid)
+            except:
+                pass
+
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.write('<html><body><form action="/login" method="post">'
+                   'Password: <input type="text" name="name">'
+                   '<input type="submit" value="Sign in">'
+                   '</form></body></html>')
+
+    def post(self):
+        self.set_secure_cookie("user", self.get_argument("name"))
+        self.redirect("/")
 
 
 class RealTimeHandler(WebSocketHandler):
@@ -142,13 +200,28 @@ class RealTimeHandler(WebSocketHandler):
     def on_close(self):
         LISTENERS.remove(self)
 
+def start_hosts(number):
+    for i in range(number):
+        create_host()
+
+def start_vms(number):
+    for m in range(number):
+        host_number_id=copy.copy(HOSTS.keys()[number-1-m])
+        for i in range(5):
+            create_vm(host_number_id,'m1.xlarge')
 
 application = Application([
     (r"/", MainHandler),
     (r"/actions/", RealTimeHandler),
-])
+    (r"/login", LoginHandler),
+], cookie_secret="61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=")
 
+run_me=True
+#run_me=False
 
 if __name__ == "__main__":
     application.listen(8888)
-    ioloop.start()
+    start_hosts(8)
+    start_vms(8)
+    if run_me:
+        ioloop.start()
